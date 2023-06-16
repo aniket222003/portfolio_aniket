2d8eb22df514478a1d40a12226c85f1044010a60
Aniket <aniketsocial22@gmail.com>
2023-06-16 13:27:27 +0530

    aniket portfolio

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#element', {
+    strings: ['Frontend Developer', 'Competitive Programmer'],
+    typeSpeed: 80,
+    backSpeed: 60,
+    loop:true,
+    
+});
+
+const btn = document.getElementById("menu");
+console.log(btn);
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily:{
+      'roboto' : ['Roboto', 'sans-serif'],
+    },
+   
+  },
+  plugins: [
+    
+  ],
+}
+
